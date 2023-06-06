624709a58afcb71529f20dde7ccde9b1fea612e9
Ahmed-Gamal-g <you@example.com>
2023-06-06 12:11:23 +0300

    initial commit

---
+++ js/Registeration.js
@@ -0,0 +1,12 @@
+// function sendEmail() {
+//     Email.send({
+//       SecureToken: "0c965f2c-f11f-4793-96de-9dffc1daf7dc",
+//       To: "gemyisking@gmail.com",
+//       From: "gemyisking@gmail.com",
+//       Subject: "test email",
+//       Body: "name : "+ document.getElementById("name").value
+//       + "<br> Email: " + document.getElementById('email').value
+//       + "<br> phone: " + document.getElementById('phone').value
+//       + "<br> message: " + document.getElementById('message').value
+//     }).then((message) => alert("message"));
+//   }
